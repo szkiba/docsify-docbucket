041f3650a6a3d5fa9eb432fbcda97df47ed2fa22
Ivan Szkiba <szkiba@users.noreply.github.com>
2020-01-24 14:05:25 +0100

    front matter dependent logo and icon support

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
+  entry: {
+    'docsify-docbucket.min.js': [
+      path.resolve(__dirname, 'src/index.js'),
+      path.resolve(__dirname, 'src/extra.js')
+    ]
+  },  
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'docsify-docbucket.min.js'
